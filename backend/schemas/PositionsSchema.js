25639b22d61684767d1a73047f1e9fe395221051
bhatia1289 <abhishekbhatia9560@gmail.com>
2025-07-17 11:25:02 +0530

    initial DB routes

--- backend/schemas/PositionsSchema.js
+++ backend/schemas/PositionsSchema.js
@@ -1,12 +1,14 @@
 const { Schema } = require("mongoose");
 
-const HoldingsSchema = new Schema({
+const PositionsSchema = new Schema({
+  product: String,
   name: String,
   qty: Number,
   avg: Number,
   price: Number,
   net: String,
   day: String,
+  isLoss: Boolean,
 });
 
-module.exports = { HoldingsSchema };
+module.exports = { PositionsSchema };
