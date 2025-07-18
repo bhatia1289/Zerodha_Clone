3ed80a71f36a955ca731b773fcd880f71070eebd
bhatia1289 <abhishekbhatia9560@gmail.com>
2025-07-18 11:25:20 +0530

    orders data and holdings graphs

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,16 +1,48 @@
 import React, { useState, useEffect } from "react";
-// import { holdings } from "../data/data";
+import axios, { all } from "axios";
+import { VerticalGraph } from "./VerticalGraph";
 
-import axios from "axios";
+// import { holdings } from "../data/data";
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3002/allHoldings").then((res) => {
+      // console.log(res.data);
       setAllHoldings(res.data);
     });
   }, []);
+
+  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = allHoldings.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allHoldings.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  // export const data = {
+  //   labels,
+  //   datasets: [
+  // {
+  //   label: 'Dataset 1',
+  //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+  // },
+  //     {
+  //       label: 'Dataset 2',
+  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+  //     },
+  //   ],
+  // };
 
   return (
     <>
@@ -71,6 +103,7 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   );
 };
--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+
+import axios from "axios";
+
+import GeneralContext from "./GeneralContext";
+
 import { Tooltip, Grow } from "@mui/material";
-import { watchlist } from "../data/data";
+
 import {
   BarChartOutlined,
   KeyboardArrowDown,
@@ -8,7 +13,66 @@ import {
   MoreHoriz,
 } from "@mui/icons-material";
 
+import { watchlist } from "../data/data";
+import { DoughnutChart } from "./DoughnoutChart";
+
+const labels = watchlist.map((subArray) => subArray["name"]);
+
 const WatchList = () => {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Price",
+        data: watchlist.map((stock) => stock.price),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // export const data = {
+  //   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  // datasets: [
+  //   {
+  //     label: "# of Votes",
+  //     data: [12, 19, 3, 5, 2, 3],
+  //     backgroundColor: [
+  //       "rgba(255, 99, 132, 0.2)",
+  //       "rgba(54, 162, 235, 0.2)",
+  //       "rgba(255, 206, 86, 0.2)",
+  //       "rgba(75, 192, 192, 0.2)",
+  //       "rgba(153, 102, 255, 0.2)",
+  //       "rgba(255, 159, 64, 0.2)",
+  //     ],
+  //     borderColor: [
+  //       "rgba(255, 99, 132, 1)",
+  //       "rgba(54, 162, 235, 1)",
+  //       "rgba(255, 206, 86, 1)",
+  //       "rgba(75, 192, 192, 1)",
+  //       "rgba(153, 102, 255, 1)",
+  //       "rgba(255, 159, 64, 1)",
+  //     ],
+  //     borderWidth: 1,
+  //   },
+  // ],
+  // };
+
   return (
     <div className="watchlist-container">
       <div className="search-container">
@@ -19,7 +83,7 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts">{watchlist.length} / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
       <ul className="list">
@@ -27,6 +91,8 @@ const WatchList = () => {
           return <WatchListItem stock={stock} key={index} />;
         })}
       </ul>
+
+      <DoughnutChart data={data} />
     </div>
   );
 };
@@ -39,9 +105,11 @@ const WatchListItem = ({ stock }) => {
   const handleMouseEnter = (e) => {
     setShowWatchlistActions(true);
   };
+
   const handleMouseLeave = (e) => {
     setShowWatchlistActions(false);
   };
+
   return (
     <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="item">
@@ -62,6 +130,12 @@ const WatchListItem = ({ stock }) => {
 };
 
 const WatchListActions = ({ uid }) => {
+  const generalContext = useContext(GeneralContext);
+
+  const handleBuyClick = () => {
+    generalContext.openBuyWindow(uid);
+  };
+
   return (
     <span className="actions">
       <span>
@@ -70,6 +144,7 @@ const WatchListActions = ({ uid }) => {
           placement="top"
           arrow
           TransitionComponent={Grow}
+          onClick={handleBuyClick}
         >
           <button className="buy">Buy</button>
         </Tooltip>
@@ -91,12 +166,7 @@ const WatchListActions = ({ uid }) => {
             <BarChartOutlined className="icon" />
           </button>
         </Tooltip>
-        <Tooltip
-          title="Analytics (A)"
-          placement="top"
-          arrow
-          TransitionComponent={Grow}
-        >
+        <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
           <button className="action">
             <MoreHoriz className="icon" />
           </button>
